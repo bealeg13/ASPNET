2912724342eed30f5d3226923d9007755c9056bb
Gabriel Bealer <bealeg13@gmail.com>
2023-10-04 09:56:54 -0700

    finished first exercise

---
+++ Testing/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Testing.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository repo;
+        public IActionResult Index()
+        {
+            var products = repo.GetAllProducts();
+            return View(products);
+        }
+
+        public ProductController(IProductRepository repo)
+        {
+            this.repo = repo;
+        }
+
+
+        
+    }
+}
